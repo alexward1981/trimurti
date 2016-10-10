a6e730031641ab2660a0eec45aa8a420b4682356
Alex Ward <alex@volcaniccreations.com>
2016-10-10 14:33:29 +0100

    added first stage of inquirer questions

--- src/static/scripts/VulcanStart.js
+++ src/static/scripts/VulcanStart.js
@@ -19,13 +19,48 @@ export default class VulcanStart {
         type: 'input',
         name: 'Name',
         message: 'What do you want to name your new Vulcan app?',
-        default: 'MYAPP'
+        default: 'MYAPP',
+        validate: function (value) {
+          var pass = value.match(/^[a-zA-Z0-9_]+$/);
+          if (pass) {
+            return true;
+          }
+
+          return 'Please do not use spaces or special characters (only A-Z a-z 0-9 and \'_\')';
+        }
       },
+      {
+        type: 'input',
+        name: 'API',
+        message: 'What is the URL of your API entrypoint (without a trailing /)?:',
+        default: 'http://localhost:8080/api'
+      },
+      {
+        type: 'input',
+        name: 'Routes',
+        message: 'Please list your API routes (not including your entrypoint) as a comma seperated list? e.g. people, places, sausage-types',
+        filter: function (val) {
+          return val.toLowerCase().replace(/ /g, '');
+        }
+      },
+      {
+        type: 'confirm',
+        name: 'Styles',
+        message: 'Do you want to import the default editor theme?:',
+        default: true
+      }
     ]
 
     inquirer.prompt(questions)
     .then(function (answers) {
-      console.log('Brill!, you have have a new Vulcan app named', chalk.cyan(answers.Name));
+      console.log('Brill!, you are creating a new Vulcan app named', chalk.cyan(answers.Name));
+      let styles_response = (answers.Styles) ? 'do' : 'don\'t';
+      console.log('You have said that you', chalk.cyan(styles_response), 'want to use the default editor theme');
+      console.log('Vulcan will now build your editor from', chalk.cyan(answers.API), 'using the following routes:');
+      let routes = answers.Routes.split(',');
+      for (var i = 0; i < routes.length; i++) {
+        console.log(chalk.cyan(answers.API +'/'+routes[i]+'/'));
+      }
     });
   }
 
